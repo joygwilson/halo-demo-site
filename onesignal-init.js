525e817bfcab025b52a3638f65d2c21a2ee43288
joygwilson <joy@onesignal.com>
2026-03-10 17:58:43 -0700

    Initialize OneSignal and add prompt function

--- onesignal-init.js
+++ onesignal-init.js
@@ -1,1 +1,16 @@
+window.OneSignalDeferred = window.OneSignalDeferred || [];
 
+OneSignalDeferred.push(async function (OneSignal) {
+  await OneSignal.init({
+    appId: "YOUR_ONESIGNAL_APP_ID",
+  });
+
+  OneSignal.Debug.setLogLevel("trace");
+});
+
+window.triggerOneSignalPrompt = function () {
+  window.OneSignalDeferred = window.OneSignalDeferred || [];
+  OneSignalDeferred.push(async function (OneSignal) {
+    OneSignal.Slidedown.promptPush({ force: true });
+  });
+};
